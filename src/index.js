506799a4f3333f70eece85884fc28355fe36551c
Henrique Duarte Rosa <l51923@alunos.uevora.pt>
2025-01-27 13:50:34 +0000

    add routing with react-router-dom and create Otherpage component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+import App from './App';
+import Login from './authentication/login';
+import Otherpage from './otherpage';
+
+import './index.css';
+
+//######## ROUTING ##########
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Login/> 
+},{
+  path: '/otherpage',
+  element: <Otherpage/>
+}])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+
+  <RouterProvider router={router} />
+
+
 
 );
 
