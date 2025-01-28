790275fa829391724274a312580320b5f8b6f362
Henrique Duarte Rosa <l51923@alunos.uevora.pt>
2025-01-28 12:43:11 +0000

    feat: add Firebase configuration and CI/CD pipeline for deployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,15 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration, using env. variables for security
 const firebaseConfig = {
-  apiKey: "AIzaSyAtyA0zfzCQiFUVvWfIOdIR1Z6WZMQp54Q",
-  authDomain: "movie-list-tdw.firebaseapp.com",
-  projectId: "movie-list-tdw",
-  storageBucket: "movie-list-tdw.firebasestorage.app",
-  messagingSenderId: "436669540677",
-  appId: "1:436669540677:web:cdff6952450533effe762c"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
+  
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
